e218b896d518a563247b3a16c5275a110d6e3fbd
desking <dimakvashenko@mail.ru>
2017-05-18 16:38:48 +0600

    Update and rename MySets.java to AnalogSet.java

--- settest/src/com/company/set/AnalogSet.java
+++ settest/src/com/company/set/AnalogSet.java
@@ -6,7 +6,7 @@ import java.util.AbstractCollection;
 /**
  * Created by dima on 15.05.17.
  */
-public interface MySets {
+public interface AnalogSet {
     boolean add(String str);
 
     boolean remove(String str);
